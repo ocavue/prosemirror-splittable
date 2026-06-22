2ec678641b65dbd1d1883c7c026f3635d68ddd7f
ocavue <ocavue@users.noreply.github.com>
2026-06-22 16:58:03 +1000

    feat!: migrate to ESM-only (#7)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
+  platform: 'browser',
   dts: { build: true },
-  fixedExtension: false,
 })
